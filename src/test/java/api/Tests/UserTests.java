223df575e5b9102b10a42672a3ead7258f7e5b42
SagarSir1992 <sgrchdhr82@gmail.com>
2023-12-10 11:54:03 +0530

    "Commit after DD test"

--- src/test/java/api/Tests/UserTests.java
+++ src/test/java/api/Tests/UserTests.java
@@ -1,6 +1,5 @@
 package api.Tests;
 
-
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
@@ -19,79 +18,75 @@ public class UserTests {
 	Faker faker;
 	User userPayload;
 	public Logger logger;
-	
-	@BeforeClass 
+
+	@BeforeClass
 	public void setUpData() {
-	    faker = new Faker();
-	    userPayload = new User();
-	    
-	    userPayload.setId(faker.idNumber().hashCode());
-	    userPayload.setUsername(faker.name().username());
-	    userPayload.setFirstName(faker.name().firstName());
-	    userPayload.setLastName(faker.name().lastName());
-	    userPayload.setEmail(faker.internet().safeEmailAddress());
-	    userPayload.setPassword(faker.internet().password(5, 10));
-	    userPayload.setPhone(faker.phoneNumber().cellPhone());
-	    
-	    //Logs
-	    logger=LogManager.getLogger(this.getClass());
-	    
-	    	
-	
+		faker = new Faker();
+		userPayload = new User();
+
+		userPayload.setId(faker.idNumber().hashCode());
+		userPayload.setUsername(faker.name().username());
+		userPayload.setFirstName(faker.name().firstName());
+		userPayload.setLastName(faker.name().lastName());
+		userPayload.setEmail(faker.internet().safeEmailAddress());
+		userPayload.setPassword(faker.internet().password(5, 10));
+		userPayload.setPhone(faker.phoneNumber().cellPhone());
+
+		// Logs
+		logger = LogManager.getLogger(this.getClass());
+
 	}
 
-	
-	@Test(priority=1)
-	public void testPostUser(){
-		 logger.debug("This is a debug message");
+	@Test(priority = 1)
+	public void testPostUser() {
+		logger.debug("This is a debug message");
 		logger.info("*********Creating User*********");
-		Response response=UserEndpoints.createUser(userPayload);
+		Response response = UserEndpoints.createUser(userPayload);
 		response.then().log().all();
-		
+
 		Assert.assertEquals(response.getStatusCode(), 200);
 		logger.info("*********User Created*********");
-	}	
+	}
 
-	@Test(priority=2)
-	public void testGetUser(){
-		 logger.debug("This is a debug message");
+	@Test(priority = 2)
+	public void testGetUser() {
+		logger.debug("This is a debug message");
 		logger.info("*********Reading User*********");
-		Response response=UserEndpoints.readUser(this.userPayload.getUsername());
+		Response response = UserEndpoints.readUser(this.userPayload.getUsername());
 		response.then().log().all();
-		
+
 		Assert.assertEquals(response.getStatusCode(), 200);
 		logger.info("*********Userinfo is displayed*********");
-	}	
-	
-	@Test(priority=3)
-	public void testUpdateUser(){
-		 logger.debug("This is a debug message");
+	}
+
+	@Test(priority = 3)
+	public void testUpdateUser() {
+		logger.debug("This is a debug message");
 		logger.info("*********Updating User*********");
-		
+
 		userPayload.setFirstName(faker.name().firstName());
-	    userPayload.setLastName(faker.name().lastName());
-	    userPayload.setEmail(faker.internet().safeEmailAddress());
-	    userPayload.setPassword(faker.internet().password(5, 10));
-	    
-		Response response=UserEndpoints.updateUser(this.userPayload.getUsername(), userPayload);
+		userPayload.setLastName(faker.name().lastName());
+		userPayload.setEmail(faker.internet().safeEmailAddress());
+		userPayload.setPassword(faker.internet().password(5, 10));
+
+		Response response = UserEndpoints.updateUser(this.userPayload.getUsername(), userPayload);
 		response.then().log().all();
-		
+
 		Assert.assertEquals(response.getStatusCode(), 200);
-		
+
 		logger.info("*********User is Updated*********");
 	}
 
-	
-	@Test(priority=4)
-	public void testDeleteUser(){
-		 logger.debug("This is a debug message");
+	@Test(priority = 4)
+	public void testDeleteUser() {
+		logger.debug("This is a debug message");
 		logger.info("*********Deleting User*********");
-		
-		Response response=UserEndpoints.deleteUser(this.userPayload.getUsername());
+
+		Response response = UserEndpoints.deleteUser(this.userPayload.getUsername());
 		response.then().log().all();
-		
+
 		Assert.assertEquals(response.getStatusCode(), 200);
-		
+
 		logger.info("*********User Deleted*********");
-	}	
+	}
 }
