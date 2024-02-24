94378bb915d929a298a005976dad9689556e10fd
Sagar-111 <sgrchdhr82@gmail.com>
2024-02-24 18:44:08 +0530

    "Done"

--- src/test/java/api/Utilities/ExtentReportManager.java
+++ src/test/java/api/Utilities/ExtentReportManager.java
@@ -13,25 +13,26 @@ import com.aventstack.extentreports.Status;
 import com.aventstack.extentreports.reporter.ExtentSparkReporter;
 import com.aventstack.extentreports.reporter.configuration.Theme;
 
-public class ExtentReportManager implements ITestListener  {
+public class ExtentReportManager implements ITestListener {
 
 	public ExtentSparkReporter sparkReporter;
 	public ExtentReports extent;
 	public ExtentTest test;
-	
+
 	String repName;
+
 	public void onStart(ITestContext testContext) {
-		
-		String timeStamp=new SimpleDateFormat("yyyy.MM.dd.HH.mm.ss").format(new Date());
-		repName="Test-Report-"+timeStamp+".html";	
-		
-		sparkReporter=new ExtentSparkReporter(".\\reports\\"+repName);
-		
+
+		String timeStamp = new SimpleDateFormat("yyyy.MM.dd.HH.mm.ss").format(new Date());
+		repName = "Test-Report-" + timeStamp + ".html";
+
+		sparkReporter = new ExtentSparkReporter(".\\reports\\" + repName);
+
 		sparkReporter.config().setDocumentTitle("RestAssured project report");
 		sparkReporter.config().setReportName("Pet store user APIs");
-		sparkReporter.config().setTheme(Theme.DARK);		
-		
-		extent=new ExtentReports();
+		sparkReporter.config().setTheme(Theme.DARK);
+
+		extent = new ExtentReports();
 		extent.attachReporter(sparkReporter);
 		extent.setSystemInfo("Application", "Pet store user APIs");
 		extent.setSystemInfo("Operating System", System.getProperty("os.name"));
@@ -40,32 +41,32 @@ public class ExtentReportManager implements ITestListener  {
 		extent.setSystemInfo("Application", "Pet store user APIs");
 		extent.setSystemInfo("User", "Sagar Choudhari");
 	}
-	
+
 	public void onTestSuccess(ITestResult result) {
-		test=extent.createTest(result.getName());
+		test = extent.createTest(result.getName());
 		test.assignCategory(result.getMethod().getGroups());
 		test.createNode(result.getName());
 		test.log(Status.PASS, "Test pass");
 	}
-	
+
 	public void onTestFailure(ITestResult result) {
-		test=extent.createTest(result.getName());
+		test = extent.createTest(result.getName());
 		test.createNode(result.getName());
 		test.assignCategory(result.getMethod().getGroups());
 		test.log(Status.FAIL, "Test Failed");
 		test.log(Status.FAIL, result.getThrowable().getMessage());
 	}
-	
+
 	public void onTestSkipped(ITestResult result) {
-		test=extent.createTest(result.getName());
+		test = extent.createTest(result.getName());
 		test.assignCategory(result.getMethod().getGroups());
 		test.createNode(result.getName());
 		test.log(Status.SKIP, "Test Skipped");
 		test.log(Status.SKIP, result.getThrowable().getMessage());
 	}
-	
+
 	public void onFinish(ITestContext testContext) {
 		extent.flush();
 	}
-	
+
 }
